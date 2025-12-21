36a4fcdd1650392b104587b3c692c8af0691ae17
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-12-21 12:42:22 +0100

    Include sora font, add data.json, and TypingForm.css

--- src/TypingForm.jsx
+++ src/TypingForm.jsx
@@ -1,25 +1,62 @@
-import React, {Fragment} from 'react';
+import {Fragment, useEffect, useState} from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import TextareaAutosize from '@mui/material/TextareaAutosize';
+import Box from '@mui/material/Box';
+import { paragraph } from 'txtgen';
+import data from './data.json';
+import './TypingForm.css';
 
 function TypingForm() {
+    // const [text] = useState(() => paragraph(4));
+    const [text] = useState(data.hard[0].text)
+    const [typed, setTyped] = useState("");
+
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.key.length === 1 && !event.ctrlKey && !event.metaKey) {
+                setTyped((prev) => prev + event.key);
+            } else if (event.key === "Backspace") {
+                setTyped((prev) => prev.slice(0, -1));
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, []);
+
   return (
     <Fragment>
       <CssBaseline />
       <Container fixed sx={{
+        fontFamily: "Sora",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
         marginTop: 10
       }}>
-            <TextareaAutosize
-            aria-label="minimum height"
-            minRows={10}
-            placeholder=""
-            style={{ width: 800 }}
-            />
+        <Box sx={{
+          bgcolor: 'hsl(0, 0%, 7%)',
+          height: '50vh',
+          width: '80vw',
+          padding: 5,
+          fontSize: 24,
+          overflowY: 'auto'
+          }}>
+            {
+              text.split("").map((char, i) => {
+                let classes = `normalStyle`
+                if (typed[i] !== null && i < typed.length){
+                  classes += typed[i] === char ? ` correctStyle` : ` errorStyle`
+                }
+                return (
+                <span key={i} className={classes}>{char}</span>
+                )
+              })
+            } 
+        </Box>
       </Container>
     </Fragment>
     
