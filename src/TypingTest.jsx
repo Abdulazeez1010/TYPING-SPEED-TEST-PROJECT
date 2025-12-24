0a1687bfc61eac330a33128fcc519735130e5f21
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-12-24 14:18:32 +0100

    feat: implement restart functionality and typing test styles

--- src/TypingTest.jsx
+++ src/TypingTest.jsx
@@ -4,11 +4,16 @@ import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import data from './data.json';
-import './TypingText.css';
+import Button from '@mui/material/Button';
+import RestartIcon from './assets/images/icon-restart.svg'
 
 import TypingText from "./TypingText";
 import TypingAppBar from "./TypingAppBar";
 import TypingMenuBar from './TypingMenuBar';
+
+import './TypingTest.css';
+
+const TEST_DURATION = 60;
 
 const statConfig = [
     { id: 'wpm', label : 'WPM',  color: 'hsl(0, 0%, 100%)', unit: ''},
@@ -20,8 +25,9 @@ function TypingTest() {
   const [difficulty, setDifficulty] = useState('hard');
   const [text, setText] = useState('')
   const [typed, setTyped] = useState('');
-  const [timeLeft, setTimeLeft] = useState(60);
+  const [timeLeft, setTimeLeft] = useState(TEST_DURATION);
   const [isRunning, setIsRunning] = useState(false);
+  const [testId, setTestId] = useState(0);
 
   const wrongLetters = typed
     .split("")
@@ -31,7 +37,7 @@ function TypingTest() {
     ? 100
     : Math.round(((typed.length - wrongLetters)/typed.length) * 100)
   
-  const minutesElapsed = (60 - timeLeft) / 60;
+  const minutesElapsed = (TEST_DURATION - timeLeft) / 60;
 
   const wpm = minutesElapsed > 0
     ? Math.round((typed.length / 5) / (minutesElapsed))
@@ -47,14 +53,15 @@ function TypingTest() {
     const passages = data[difficulty];
     const randomIndex = Math.floor(Math.random() * passages.length)
     setText(passages[randomIndex].text)
-
     setTyped('');
-    setTimeLeft('60');
+    setTimeLeft(TEST_DURATION);
     setIsRunning(false);
-  }, [difficulty]);
+  }, [difficulty, testId]);
 
   useEffect(() => {
     const handleKeyDown = (event) => {
+      // if (!isRunning && typed.length > 0) return;
+      if (event.key === ' ') event.preventDefault();
       if (timeLeft === 0) return;
 
       if (event.key.length === 1 && !event.ctrlKey && !event.metaKey) {
@@ -63,6 +70,7 @@ function TypingTest() {
         setTyped((prev) => prev.slice(0, -1));
       }
     };
+    
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
   }, [timeLeft]);
@@ -89,10 +97,13 @@ function TypingTest() {
     return () => clearInterval(interval);
   }, [isRunning]);
 
-  const updateDifficulty= (lvl) => {
+  const updateDifficulty = (lvl) => {
     setDifficulty(lvl);
   }
 
+  const restart = () => {
+    setTestId(prev => prev + 1);
+  }
 
   return (
     <>
@@ -110,7 +121,7 @@ function TypingTest() {
           width: '80vw',
           padding: "1rem 5rem",
           fontSize: 24,
-          overflowY: 'auto'
+          // overflowY: 'auto'
           }}
         >
           <TypingAppBar/>
@@ -119,13 +130,27 @@ function TypingTest() {
             stats={stats}
             updateDifficulty={updateDifficulty}
           />
-        <Divider sx={{borderColor: 'hsl(240, 1%, 59%)', opacity: 0.3}}/>
+          <Divider sx={{borderColor: 'hsl(240, 1%, 59%)', opacity: 0.3}}/>
         
-        <TypingText
-          text={text}
-          typed={typed}
-          isRunning
-        />
+          <TypingText
+            text={text}
+            typed={typed}
+            isRunning={isRunning}
+          />
+          <Divider sx={{borderColor: 'hsl(240, 1%, 59%)', opacity: 0.3}}/>
+          <div 
+            id='TypingTest-button'
+            style={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              margin: '0.5rem'
+              }}
+            >
+            <Button onClick={restart} >
+              Restart Test <img src={RestartIcon} alt='Typing Test Logo'/>
+            </Button>
+          </div>
         </Box>
       </Container>
     </Fragment>        
